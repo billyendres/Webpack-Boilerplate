2762369fc985af779c5828ef78228111235eb0cb
Billy Endres <billy@koben.com.au>
2022-04-02 22:55:26 +1100

    Setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const App = () => {
+  return (
+    <div>
+      <H1>App</H1>
+    </div>
+  );
+};
+
+export default App;
+
+const H1 = styled.h1`
+  color: blue;
+`;
